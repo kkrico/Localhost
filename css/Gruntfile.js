1ba454e3b5fc814a1294655087fb66ab20bc1bdb
Daniel Ramos <ramos.danielferreira@gmail.com>
2017-06-27 20:52:25 -0300

    Montado ambiente css

---
+++ css/Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+
+        watch: {
+            cssmin: {
+                files: 'styles/styles.css',
+                options: {
+                    livereload: 35729
+                }
+            },
+             all: {
+                files: ['**/*.html'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+
+        connect: {
+            server: {
+
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+
+    grunt.registerTask('default', ['connect','watch']);
+};
